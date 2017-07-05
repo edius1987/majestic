ed6516dfb07e2a1c92d007091e301d2c7d5cad67
Lucas Salies Brum <lucas@archlinux.com.br>
2017-07-04 20:33:15 -0400

    new file:   home/lucas/.Xresources 	modified:   home/lucas/.config/compton.conf 	modified:   home/lucas/.config/i3/blocks 	modified:   home/lucas/.config/i3/config 	modified:   home/lucas/.config/i3/scripts/volume 	modified:   home/lucas/.config/i3/sistematico.dunstrc 	new file:   home/lucas/.config/systemd/user/xautolock.service 	modified:   home/lucas/.config/termite/config 	copied:     home/lucas/.config/termite/config -> home/lucas/.config/termite/config.old 	deleted:    home/lucas/bin/firefox-sync 	modified:   home/lucas/bin/screencast.sh 	modified:   home/lucas/bin/screenshot.sh 	new file:   usr/local/bin/app.live

--- home/lucas/bin/screencast.sh
+++ home/lucas/bin/screencast.sh
@@ -5,7 +5,7 @@
 # Desenvolvido por Lucas Saliés Brum <lucas@archlinux.com.br>
 #
 # Criado em: 09/06/2017
-# Última Atualização: 10/06/2017 09:36:56 
+# Última Atualização: 10/06/2017 09:36:56
 #
 
 nome="screencast"
@@ -31,21 +31,21 @@ if [ "$1" == "-s" ]; then
 elif [ "$1" == "-a" ]; then
 	tmux a -t $nome
 elif [ "$1" == "-h" ]; then
-	echo	
+	echo
 	echo "Uso: $0 [-hs]"
 	echo
 else
 	if [ "$ativo" != "screencast" ]; then
 		# tmux new-session -d -s $nome "ffmpeg -f x11grab -video_size $resolucao -framerate 30 -i :0.0 -f pulse -i default -preset $preset -crf 18 -pix_fmt yuv420p $arquivo"
 		# tmux new-session -d -s $nome "ffmpeg -f pulse -i $audio -f x11grab -r 30 -s $resolucao -i :0.0 -acodec pcm_s16le -vcodec libx264 -preset $preset -crf 0 -threads 0 $arquivo"
-		if [ "$2" == "-t" ]; then		
+		if [ "$2" == "-t" ]; then
 			tmux new-session -d -s $nome "ffmpeg -f x11grab -r 30 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -crf 0 -threads 0 -probesize 10M $arquivo"
 		else
 			#ffmpeg -f x11grab -r 25 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -crf 0 -threads 0 -probesize 10M $arquivo
 			ffmpeg -f x11grab -r 25 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -vcodec libx264 -probesize 30M $arquivo
 		fi
 		notify-send -i $icone "ScreenCast" "O vídeo <b>$arquivo</b> foi iniciado..."
-	else	
+	else
 		notify-send -i $icone "ScreenCast" "Uma sessão já está ativa, digite <b>$0 -s</b> para parar!"
 	fi
 fi
--- home/lucas/bin/screenshot.sh
+++ home/lucas/bin/screenshot.sh
@@ -2,19 +2,30 @@
 
 dir="${HOME}/img/shots/"
 data=$(date +%Y-%m-%d_%H-%M-%S)
-nome="ss-${data}.png"
+nome="ss-${data}"
+extensao=".png"
+icone="/usr/share/icons/Arc/devices/48@2x/video-display.png"
+
+if [ ! -d $dir ]; then
+	mkdir -p $dir
+fi
 
 if [ "$1" == "-a" ]; then
-	scrot -m $nome
+	arquivo="${nome}-full${extensao}"
+	scrot -m ${arquivo}
 elif [ "$1" == "-w" ]; then
-	scrot -u -m $nome
+	arquivo="${nome}-window${extensao}"
+	scrot -u -m ${arquivo}
 else
-	scrot -m $nome
-	mogrify -crop 1920x1080+0+0 +repage $nome
+	arquivo="${nome}-crop${extensao}"
+	scrot -m ${arquivo}
+	mogrify -crop 1920x1080+0+0 +repage ${arquivo}
 fi
 
 if [ $(pwd) != $dir ]; then
-	mv $nome $dir
+	mv $arquivo $dir
 fi
 
 xclip -selection clipboard -t image/png -i $dir$nome
+
+notify-send -i $icone "ScreenShot" "A screenshot <b>$arquivo</b> foi salva em <b>$dir</b>..."
