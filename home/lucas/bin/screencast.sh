07fa17c982b4eccfe2bd407412b6123450d2b98d
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-07-18 12:27:30 -0400

    modified:   home/lucas/.config/i3/blocks 	new file:   home/lucas/.config/i3/blocks.tst 	modified:   home/lucas/.config/i3/config 	modified:   home/lucas/.config/i3/scripts/focus 	copied:     home/lucas/.config/i3/scripts/focus -> home/lucas/.config/i3/scripts/focus.old 	modified:   home/lucas/.config/i3/scripts/mpd 	deleted:    home/lucas/.config/systemd/user/default.target.wants/mpd.service 	new file:   home/lucas/.config/systemd/user/graphical.target.wants/xautolock.service 	modified:   home/lucas/bin/screencast.sh

--- home/lucas/bin/screencast.sh
+++ home/lucas/bin/screencast.sh
@@ -19,13 +19,18 @@ resolucao=$(xrandr | grep '*' | awk 'NR==1{print $1}')
 # alsa_output.usb-Logitech_Logitech_G930_Headset-00.analog-stereo.monitor
 audio=$(pacmd list-sinks | grep -A 1 'index: 1' | awk 'NR==2{print $2}' | awk '{print substr($0,2,length($0)-2)}') # list-sources, list-sinks
 ativo=$(tmux ls 2> /dev/null | grep $nome | cut -d ':' -f 1)
+ativo2=$(ps -A | grep ffmpeg 2>&1)
 
 if [ "$1" == "-s" ]; then
 	if [ "$ativo" == "screencast" ]; then
 		tmux kill-session -t $nome
 		killall ffmpeg
 		notify-send -i $icone "ScreenCast" "O vídeo <b>$arquivo</b> foi terminado."
+	elif [ "$ativo2" ]; then
+		killall ffmpeg
+		notify-send -i $icone "ScreenCast" "O vídeo <b>$arquivo</b> foi terminado."
 	else
+
 		notify-send -i $icone "ScreenCast" "Nenhuma sessão ativa, digite <b>$0</b> para iniciar!"
 	fi
 elif [ "$1" == "-a" ]; then
@@ -36,15 +41,21 @@ elif [ "$1" == "-h" ]; then
 	echo
 else
 	if [ "$ativo" != "screencast" ]; then
-		# tmux new-session -d -s $nome "ffmpeg -f x11grab -video_size $resolucao -framerate 30 -i :0.0 -f pulse -i default -preset $preset -crf 18 -pix_fmt yuv420p $arquivo"
-		# tmux new-session -d -s $nome "ffmpeg -f pulse -i $audio -f x11grab -r 30 -s $resolucao -i :0.0 -acodec pcm_s16le -vcodec libx264 -preset $preset -crf 0 -threads 0 $arquivo"
-		if [ "$2" == "-t" ]; then
-			tmux new-session -d -s $nome "ffmpeg -f x11grab -r 30 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -crf 0 -threads 0 -probesize 10M $arquivo"
+		if [ ! $ativo2 ]; then
+			notify-send -i $icone "ScreenCast" "O vídeo <b>$arquivo</b> foi iniciado..."
+
+			# tmux new-session -d -s $nome "ffmpeg -f x11grab -video_size $resolucao -framerate 30 -i :0.0 -f pulse -i default -preset $preset -crf 18 -pix_fmt yuv420p $arquivo"
+			# tmux new-session -d -s $nome "ffmpeg -f pulse -i $audio -f x11grab -r 30 -s $resolucao -i :0.0 -acodec pcm_s16le -vcodec libx264 -preset $preset -crf 0 -threads 0 $arquivo"
+			if [ "$2" == "-t" ]; then
+				#tmux new-session -d -s $nome "ffmpeg -f x11grab -r 30 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -crf 0 -threads 0 -probesize 10M $arquivo"
+				tmux new-session -d -s $nome "ffmpeg -f x11grab -r 30 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -crf 0 -threads 0 -c:v libx264 -b:v 464k -c:a aac -b:a 128k -probesize 10M $arquivo"
+			else
+				#ffmpeg -f x11grab -r 25 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -crf 0 -threads 0 -probesize 10M $arquivo
+				ffmpeg -f x11grab -r 25 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -c:v libx264 -c:a aac -b:a 128k -probesize 30M $arquivo
+			fi
 		else
-			#ffmpeg -f x11grab -r 25 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -crf 0 -threads 0 -probesize 10M $arquivo
-			ffmpeg -f x11grab -r 25 -s $resolucao -i :0.0 -f pulse -i default -preset $preset -vcodec libx264 -probesize 30M $arquivo
+			notify-send -i $icone "ScreenCast" "Uma sessão já está ativa, digite <b>$0 -s</b> para parar!"
 		fi
-		notify-send -i $icone "ScreenCast" "O vídeo <b>$arquivo</b> foi iniciado..."
 	else
 		notify-send -i $icone "ScreenCast" "Uma sessão já está ativa, digite <b>$0 -s</b> para parar!"
 	fi
