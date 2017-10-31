da24963a03c7bbbd94c5a47f6ad33c4a7f832878
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-10-31 13:20:15 -0300

    modified:   home/lucas/.config/dunst/dunstrc 	modified:   home/lucas/.config/rofi/scripts/power 	new file:   home/lucas/.config/sublime-text-3/Packages/User/bootstrap4.sublime-snippet 	new file:   home/lucas/.config/sublime-text-3/Packages/User/html5.sublime-snippet 	modified:   home/lucas/bin/alarme.sh 	new file:   home/lucas/bin/corrupted_video.sh

--- home/lucas/bin/alarme.sh
+++ home/lucas/bin/alarme.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-n=1
-while (( $n <= 10 ))
-do
-	paplay ~/audio/alarmes/naval/cvn77whistle.wav
-    sleep 10
-	n=$(( n+1 ))
- done
+paplay ~/audio/alarmes/naval/cvn77whistle.wav
+
+#n=1
+#while (( $n <= 10 ))
+#do
+#	paplay ~/audio/alarmes/naval/cvn77whistle.wav
+#    sleep 10
+#	n=$(( n+1 ))
+# done
 
 #while : ; do
 #	paplay ~/audio/alarmes/naval/cvn77whistle.wav
