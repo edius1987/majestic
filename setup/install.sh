9f8e783c70a046195c883f3dee1679f7aa9a8746
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-11-11 22:14:41 -0300

    modified:   setup/install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -2,7 +2,7 @@
 
 [ "$EUID" -eq 0 ] && echo "É necessário rodar o script como usuário normal, não como root." && exit 1
 app_ok=1
-apps=("i3-gaps" "i3blocks" "i3lock-fancy" "dunst" "conky" "xdotool" "cantarell-fonts" "ttf-hack" "ttf-iosevka" "alsa-utils" "maim" "xclip")
+apps=("i3-gaps" "i3blocks" "i3lock-fancy-git" "dunst" "conky" "xdotool" "cantarell-fonts" "ttf-hack" "ttf-iosevka" "alsa-utils" "maim" "xclip")
 
 for app in ${apps[@]}; do
 	pacman -Q $app 1> /dev/null 2> /dev/null
