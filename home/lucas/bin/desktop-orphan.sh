dda86ff54be591bff4efe79d16c858b9f05ad6eb
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-11-23 21:57:46 -0300

    modified:   home/lucas/.config/i3/config 	modified:   home/lucas/.config/i3blocks/config 	modified:   home/lucas/.config/i3blocks/config.airblader 	modified:   home/lucas/.config/i3blocks/config.clean 	new file:   home/lucas/.config/i3blocks/config.minimal 	new file:   home/lucas/.config/i3blocks/scripts/cpu_usage 	modified:   home/lucas/.config/i3blocks/scripts/i3blockstheme 	modified:   home/lucas/.config/i3blocks/scripts/wallpaper 	modified:   home/lucas/.config/i3blocks/scripts/weather 	modified:   home/lucas/.config/rofi/scripts/configs 	modified:   home/lucas/.config/rofi/scripts/power 	modified:   home/lucas/.icons/default/index.theme 	new file:   home/lucas/bin/desktop-orphan.sh 	modified:   setup/install.sh

---
+++ home/lucas/bin/desktop-orphan.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Array associativa
+#declare -A apps
+# Array numerica
+declare -a apps
+
+homedir="${HOME}/.local/share/applications"
+sysdir="/usr/share/applications"
+apps=("cinnamon-" "qt4" "mate-color-select" "mpv")
+todos=($(find $sysdir $homedir -type f | egrep .desktop))
+nd='NoDisplay'
+
+#function uso {}
+
+function nodisp {
+	if [ $1 = '-r' ]; then
+        if [ -f $2 ]; then
+			grep 'NoDisplay' $2 1> /dev/null
+        	if [ $? = 0 ]; then
+            	echo "Removendo NoDisplay de $2"
+				sed -i.bak "/$nd/d" $2
+        	fi
+		fi
+	else
+		grep 'NoDisplay' $1 1> /dev/null
+		if [ $? = 1 ]; then
+			echo "Inserindo NoDisplay em $1"
+			echo 'NoDisplay=true' >> $1
+		fi
+	fi
+}
+
+if [ $1 ]; then
+	if [ $1 = '-s' ]; then
+		if [ $2 ]; then
+			arquivo=$(grep -r $2 /usr/share/applications/ | awk -F':' '{print $1}' | uniq | head -n1)
+			if [ ! -z $arquivo ]; then
+				echo $arquivo
+				echo $arquivo | xclip -r -selection c
+			else
+				echo "Nenhum arquivo encontrado."
+			fi
+		fi
+
+	elif [ $1 = '-r' ]; then
+		if [ -f "${homedir}/$(basename $2)" ]; then
+			nodisp -r "${homedir}/$(basename $2)"
+		fi
+	elif [ -f "${homedir}/$(basename $1)" ]; then
+		nodisp "${homedir}/$(basename $1)"
+	elif [ -f "${sysdir}/$(basename $1)" ]; then
+		cp "${sysdir}/$(basename $1)" "${homedir}/"
+		nodisp "${homedir}/$(basename $1)"
+	fi
+	exit 0
+fi
+
+for app in ${apps[@]}; do
+	echo "Procurando o regex ${app}..."
+    for element in "${todos[@]}"; do
+        if [[ $element = *$app* ]]; then
+            nome=$(basename $element)
+			echo "O $nome coincide com ${app}..."
+
+    		if [ ! -f "${homedir}/${nome}" ]; then
+    		   echo "$nome não existe em ${homedir}"
+    		   if [ -f "${sysdir}/${nome}" ]; then
+    		       echo "$nome existe em ${sysdir}, copiando para ${homedir}/${nome}"
+    		       cp "${sysdir}/${nome}" "${homedir}/"
+    		   fi
+    		fi
+			[ -f "${homedir}/${nome}" ] && nodisp "${homedir}/${nome}"
+
+        fi
+    done
+done
