0c584b0c33373b39ef0beca4b23262e15cd4f685
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-01-07 14:12:05 -0300

    modified:   install-i3.sh 	modified:   screenshot.png

--- install-i3.sh
+++ install-i3.sh
@@ -4,7 +4,7 @@
 
 app_ok=1
 
-apps=("i3-gaps" "i3blocks" "dunst" "xdotool" "cantarell-fonts" "ttf-hack")
+apps=("i3-gaps" "i3blocks" "dunst" "xdotool" "ttf-fira-sans")
 
 for app in ${apps[@]}; do
 	pacman -Q $app 1> /dev/null 2> /dev/null
@@ -30,46 +30,72 @@ fi
 echo "Instalando as configs do i3..."
 curl -s -o ${HOME}/.config/i3/config 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3/config'
 
-# i3blocks
-if [ -d ${HOME}/.config/i3blocks ]; then
-	if [ -f ${HOME}/.config/i3blocks/config ]; then
-		mv ${HOME}/.config/i3blocks/config ${HOME}/.config/i3blocks/config-${backup_date}.bkp
+if [ $i3blocks == true ]; then
+	# i3blocks
+	if [ -d ${HOME}/.config/i3blocks ]; then
+		if [ -f ${HOME}/.config/i3blocks/config ]; then
+			mv ${HOME}/.config/i3blocks/config ${HOME}/.config/i3blocks/config-${backup_date}.bkp
+		fi
+	else
+		mkdir -p ${HOME}/.config/i3blocks
 	fi
+
+	echo "Instalando as configs do i3blocks..."
+	curl -s -o ${HOME}/.config/i3blocks/config 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/config.airblader'
+	curl -s -o ${HOME}/.config/i3blocks/cores.conf 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/cores.conf'
+
+	# i3blocks scripts
+	if [ -d ${HOME}/.config/i3blocks/scripts ]; then
+		mv ${HOME}/.config/i3blocks/scripts ${HOME}/.config/i3blocks/scripts-${backup_date}
+	else
+		mkdir -p ${HOME}/.config/i3blocks/scripts
+	fi
+
+	scripts=("blank" "cpu" "disk" "firewall" "focus" "hora" "mem" "mpd"	"packages" "temperature" "trash" "uptime" "volume" "wallpaper" "weather" "wifi")
+
+	for script in ${scripts[@]}; do
+		curl -s -o ${HOME}/.config/i3blocks/scripts/${script} "https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/${script}"
+	done
 else
-	mkdir -p ${HOME}/.config/i3blocks
+	# PolyBar
+if [ -d ${HOME}/.config/i3blocks ]; then
+		if [ -f ${HOME}/.config/i3blocks/config ]; then
+			mv ${HOME}/.config/i3blocks/config ${HOME}/.config/i3blocks/config-${backup_date}.bkp
+		fi
+	else
+		mkdir -p ${HOME}/.config/i3blocks
+	fi
+
+	echo "Instalando as configs do i3blocks..."
+	curl -s -o ${HOME}/.config/i3blocks/config 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/config.airblader'
+	curl -s -o ${HOME}/.config/i3blocks/cores.conf 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/cores.conf'
+
+	# polybar scripts
+	if [ -d ${HOME}/.config/polybar/scripts ]; then
+		mv ${HOME}/.config/polybar/scripts ${HOME}/.config/polybar/scripts-${backup_date}
+	else
+		mkdir -p ${HOME}/.config/polybar/scripts
+	fi
+
+	scripts=("anews.py" "btc.sh" "crypto2.py" "crypto.py" "janela.sh" "logs.sh" "pkg.sh" "strcut.py" "trash.sh" "weather.py")
+	gmail=("auth.py"  "client_secrets.json"  "credentials.json"  "launch.py"  "preview.png"  "readme.md")
+
+	for script in ${scripts[@]}; do
+		curl -s -o ${HOME}/.config/polybar/scripts/${script} "https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/polybar/scripts/${script}"
+	done
+
+	if [ ! -d ${HOME}/.config/polybar/scripts/gmail ]; then
+		mkdir -p ${HOME}/.config/polybar/scripts/gmail
+	fi
+
+	for gm in ${gmail[@]}; do
+		curl -s -o ${HOME}/.config/polybar/scripts/gmail/${gm} "https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/polybar/scripts/gmail/${gm}"
+	done	
+
+	curl -s -o ${HOME}/.config/polybar/trans.i3 "https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/polybar/trans.i3"
+	curl -s -o ${HOME}/.config/polybar/i3.sh "https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/polybar/i3.sh"
+	ln -s ${HOME}/.config/polybar/trans.i3 ${HOME}/.config/polybar/i3
 fi
-
-echo "Instalando as configs do i3blocks..."
-curl -s -o ${HOME}/.config/i3blocks/config 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/config.airblader'
-curl -s -o ${HOME}/.config/i3blocks/cores.conf 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/cores.conf'
-
-# i3blocks scripts
-if [ -d ${HOME}/.config/i3blocks/scripts ]; then
-	mv ${HOME}/.config/i3blocks/scripts ${HOME}/.config/i3blocks/scripts-${backup_date}
-else
-	mkdir -p ${HOME}/.config/i3blocks/scripts
-fi
-
-scripts=("blank"
-		"cpu"
-		"disk"
-		"firewall"
-		"focus"
-		"hora"
-		"mem"
-		"mpd"
-		"packages"
-		"temperature"
-		"trash"
-		"uptime"
-		"volume"
-		"wallpaper"
-		"weather"
-		"wifi")
-
-for script in ${scripts[@]}; do
-	curl -s -o ${HOME}/.config/i3blocks/scripts/${script} "https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/i3blocks/${script}"
-done
 
 # dunst
 if [ -d ${HOME}/.config/dunst ]; then
@@ -77,7 +103,7 @@ if [ -d ${HOME}/.config/dunst ]; then
 		mv ${HOME}/.config/dunst/dunstrc ${HOME}/.config/dunst/dunstrc-${backup_date}.bkp
 	fi
 else
-	mkdir -p ${HOME}/.config/i3blocks/scripts
+	mkdir -p ${HOME}/.config/dunst
 fi
 
 curl -s -o ${HOME}/.config/dunst/dunstrc 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/dunst/dunstrc'
@@ -101,6 +127,8 @@ curl -s -o ${HOME}/.config/rofi/config.rasi 'https://raw.githubusercontent.com/s
 curl -s -o ${HOME}/.config/rofi/scripts/apps 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/rofi/scripts/apps'
 curl -s -o ${HOME}/.config/rofi/scripts/configs 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/rofi/scripts/configs'
 curl -s -o ${HOME}/.config/rofi/scripts/power 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/rofi/scripts/power'
+curl -s -o ${HOME}/.config/rofi/scripts/run 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/rofi/scripts/run'
+curl -s -o ${HOME}/.config/rofi/scripts/screenshot 'https://raw.githubusercontent.com/sistematico/majestic/master/home/lucas/.config/rofi/scripts/screenshot'
 
 # fontawesome
 fc-list | grep -i FontAwesome 1> /dev/null 2> /dev/null
