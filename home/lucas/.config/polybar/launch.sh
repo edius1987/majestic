65f2a0b0212b80a2638f6dedeaff9ea27f6ce442
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-01-21 04:00:02 -0300

    Autocommit por Gig Cron: alterações: add 'home/lucas/.config/polybar/clean' add 'home/lucas/.config/polybar/config' add 'home/lucas/.config/polybar/full' add 'home/lucas/.config/polybar/launch.sh' add 'home/lucas/.config/polybar/scripts/wallpaper.sh' add 'home/lucas/.config/polybar/principal'

--- home/lucas/.config/polybar/launch.sh
+++ home/lucas/.config/polybar/launch.sh
@@ -9,6 +9,17 @@ while pgrep -u $UID -x polybar >/dev/null; do
 	sleep 1;
 done
 
+if [ ! -L ${HOME}/.config/polybar/principal ]; then
+	ln -s ${HOME}/.config/polybar/clean ${HOME}/.config/polybar/principal
+fi
+
+tamanho=$(cat ${HOME}/.config/polybar/config | wc -l)
+
+if [ "$tamanho" -lt 30 ]; then
+	cat ${HOME}/.config/polybar/cores/branco > ${HOME}/.config/polybar/config
+	cat ${HOME}/.config/polybar/clean >> ${HOME}/.config/polybar/config
+fi
+
 # Launch bar1 and bar2
 polybar bottom -q &
 polybar top -q &
