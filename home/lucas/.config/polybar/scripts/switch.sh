9d4f70d53290be28fc888da1eee770a1bd2e1dbc
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-01-21 03:45:02 -0300

    Autocommit por Gig Cron: alterações: add 'home/lucas/.config/polybar/config' remove 'home/lucas/.config/polybar/i3.sh' remove 'home/lucas/.config/polybar/principal' add 'home/lucas/.config/polybar/scripts/switch.sh' remove 'home/lucas/.config/polybar/trans.i3' add 'home/lucas/.local/bin/screencast' add 'home/lucas/.config/polybar/clean' add 'home/lucas/.config/polybar/full'

--- home/lucas/.config/polybar/scripts/switch.sh
+++ home/lucas/.config/polybar/scripts/switch.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-destino=$(basename $(readlink -f ${HOME}/.config/polybar/i3))
+destino=$(basename $(readlink -f ${HOME}/.config/polybar/principal))
 cd ${HOME}/.config/polybar/
 
-if [ ${destino} == "trans-clean.i3" ]; then
+if [ ${destino} == "clean" ]; then
 	s=""
 	if [ $1 ]; then
-		ln -sf trans.i3 i3
-		~/.config/polybar/i3.sh
+		ln -sf full principal
+		~/.config/polybar/launch.sh
 	fi
 else
 	s="%{F#d08770}%{F-}"
 	if [ $1 ]; then
-		ln -sf trans-clean.i3 i3
-		~/.config/polybar/i3.sh
-	fi	
+		ln -sf clean principal
+		~/.config/polybar/launch.sh
+	fi
 fi
 
 echo "${s}"
