42bf060ddb4f6a4187945b8dc13786e0bb95219c
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-03-23 21:12:02 -0400

    Autocommit por Git-Cron: alterações: add '.config/gtk-3.0/bookmarks' add '.local/bin/git-cron' add 'bin/syncmon.sh'

--- bin/syncmon.sh
+++ bin/syncmon.sh
@@ -6,20 +6,21 @@
 # Criador por Lucas Saliés Brum a.k.a. sistematico, <lucas at archlinux dot com dot br>
 #
 # Criado em: 15/03/2018 18:15:04
-# Última alteração: 16-03-2018 18:04:10
+# Última alteração: 23-03-2018 15:13:58
 
 RSYNC=$(which rsync)
 command -v inotifywait >/dev/null 2>&1 || { echo >&2 "O aplicativo inotifywait não está instalado. Abortando."; exit 1; }
 
 if [ $2 ]; then
  	[ -d $1 ] || { echo >&2 "$1 não é uma pasta. Abortando."; exit 1; }
-	param=$1
-	tam=${#param}
-	ult=${param:tam-1:1}
-	[[ $ult != "/" ]] && param="$param/"; :
+	origem=$1
+	destino=$2
+	tam=${#origem}
+	ult=${origem:tam-1:1}
+	[[ $ult != "/" ]] && origem="$origem/"; :
 
 	while true; do
-		inotifywait -q -e modify,create,delete -r $param && $RSYNC -avz $param $2
+		inotifywait -q -e modify,create,delete -r $origem && $RSYNC -avz $origem $destino && echo "Arquivo alterado..."
 	done
 else
 	echo "Uso: $(basename $0) /pasta/local/ usuario@host:/pasta/remota/"
