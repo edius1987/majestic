ff494e23c8d8adc573192c6b9f79431cc5da26c0
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-04-01 17:10:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/slim-random-install.sh'

--- bin/slim-random-install.sh
+++ bin/slim-random-install.sh
@@ -7,6 +7,7 @@
 # Criado em: 01/04/2018 15:45:24
 # Última Atualização: 01/04/2018 15:45:29
 
+dir="/usr/share/backgrounds/slim"
 uniturl="http://ix.io/15uO"
 unitfile="/etc/systemd/system/slimbackground.service"
 slimurl="http://ix.io/15vv"
@@ -22,6 +23,14 @@ if [ "$EUID" == 0 ]; then
 		pacman -S archlinux-themes-slim
 		sudo cp -r /usr/share/slim/themes/archlinux-retro /usr/share/slim/themes/random
 	fi
+
+	if [ ! -d $dir ]; then
+		mkdir -p $dir
+		curl -L -s "https://unsplash.it/1920/1080?random" > $dir/unsplash1.jpg
+		curl -L -s "https://unsplash.it/1920/1080?random" > $dir/unsplash2.jpg
+		curl -L -s "https://unsplash.it/1920/1080?random" > $dir/unsplash3.jpg
+	fi
+
     echo "Instalando a unit $(basename $unitfile) em $(dirname $unitfile)..."
     curl -s -L -o $unitfile "$uniturl"
     echo "Instalando o script $(basename $slimfile) em $(dirname $slimfile)..."
@@ -29,7 +38,7 @@ if [ "$EUID" == 0 ]; then
     systemctl daemon-reload
     sed -i "0,/^current_theme.*/s//current_theme random/" /etc/slim.conf
     systemctl disable lightdm gdm nodm kdm
-    systemctl enable slim-background
+    systemctl enable slimbackground slim
 else
     if sudo true; then
     	if [ ! -d /usr/share/slim/themes/archlinux-retro ] && [ ! -d /usr/share/slim/themes/random ]; then
@@ -37,6 +46,14 @@ else
 			sudo pacman -S archlinux-themes-slim
 			sudo cp -r /usr/share/slim/themes/archlinux-retro /usr/share/slim/themes/random
 		fi
+
+		if [ ! -d $dir ]; then
+			sudo mkdir -p $dir
+			sudo curl -L -s "https://unsplash.it/1920/1080?random" > $dir/unsplash1.jpg
+			sudo curl -L -s "https://unsplash.it/1920/1080?random" > $dir/unsplash2.jpg
+			sudo curl -L -s "https://unsplash.it/1920/1080?random" > $dir/unsplash3.jpg
+		fi
+
 	    echo "Instalando a unit $(basename $unitfile) em $(dirname $unitfile)..."
 	    sudo curl -s -L -o $unitfile "$uniturl"
 	    echo "Instalando o script $(basename $slimfile) em $(dirname $slimfile)..."
@@ -44,7 +61,7 @@ else
 	    sudo systemctl daemon-reload
 	    sed -i "0,/^current_theme.*/s//current_theme random/" /etc/slim.conf
 	    sudo systemctl disable lightdm gdm nodm kdm
-	    sudo systemctl enable slim-background
+	    sudo systemctl enable slimbackground slim
     else
         echo "Você precisa de permissões de root."
     fi
