d8d88ae5bf0871913a2c2275a3d78c54d84c2eb1
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-04-01 16:50:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/vms.sh'

--- bin/vms.sh
+++ bin/vms.sh
@@ -5,10 +5,14 @@
 # Feito por Lucas Saliés Brum a.k.a. sistematico, <lucas@archlinux.com.br>
 #
 # Criado em: 01-04-2018 16:31:02
-# Última alteração: 01-04-2018 16:37:48
+# Última alteração: 01-04-2018 16:48:14
 
 rodando=$(VBoxManage list runningvms)
 todas=$(VBoxManage list vms)
+
+for vm in $todas; do
+	echo $vm | grep -oP "Guest OS"
+done
 
 dialog --title 'Perfil' --menu 'Escolha o perfil da instalação:' 0 0 0 mínima 'Instala o mínimo' completa     'Instala tudo' customizada  'Você escolhe'
 
