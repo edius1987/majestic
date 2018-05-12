7cddf49d7dbb6da16b18890263cde172907e7503
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-05-11 21:45:02 -0400

    Autocommit por Git-Cron: alterações: add 'bin/fresh-install.sh'

--- bin/fresh-install.sh
+++ bin/fresh-install.sh
@@ -11,8 +11,18 @@ mpv viewnior openssh vino vinagre mosh tmux rsync xorg-xinit wmctrl
 dconf write /org/gtk/settings/file-chooser/sort-directories-first true
 xfconf-query -c thunar -n -p /misc-full-path-in-title -t bool -s true
 
-
 # Fonte: https://github.com/linuxmint/cinnamon-settings-daemon/issues/141#issuecomment-304640407
-gsettings set org.cinnamon.desktop.session session-manager-uses-logind true
-gsettings set org.cinnamon.desktop.session settings-daemon-uses-logind true
-gsettings set org.cinnamon.desktop.session screensaver-uses-logind true
+#gsettings set org.cinnamon.desktop.session session-manager-uses-logind true
+#gsettings set org.cinnamon.desktop.session settings-daemon-uses-logind true
+#gsettings set org.cinnamon.desktop.session screensaver-uses-logind true
+
+# MATE
+gsettings set org.mate.Marco.general center-new-windows true
+gsettings set org.mate.caja.desktop computer-icon-visible false
+gsettings set org.mate.caja.desktop home-icon-visible false
+gsettings set org.mate.caja.desktop network-icon-visible false
+gsettings set org.mate.caja.desktop trash-icon-visible false
+gsettings set org.mate.caja.desktop volumes-visible false
+gsettings set org.mate.media-handling automount-open false
+#gsettings set org.mate.panel.menubar icon-name archlinux
+
