7cc0546404dc37dd60c1c8a5a1556a29cbf327a7
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-05-16 17:45:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/syshw.sh'

--- bin/syshw.sh
+++ bin/syshw.sh
@@ -1,94 +1,88 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 clear
 
-# define colors for color-echo
-red="\e[31m"
-grn="\e[32m"
-ylw="\e[33m"
-cyn="\e[36m"
-blu="\e[34m"
-prp="\e[35m"
-bprp="\e[35;1m"
+# Define as cores
+vermelho="\e[31m"
+verde="\e[32m"
+amarelo="\e[33m"
+ciano="\e[36m"
+azul="\e[34m"
+roxo="\e[35m"
+roxo2="\e[35;1m"
 rst="\e[0m"
 
-color-echo()
-{  # print with colors
-  echo -e " $red$1: $rst$2"
+cor-echo() {  # imprime as as cores
+  echo -e " $vermelho$1: $rst$2"
 }
 
-print-kernel()
-{
-  color-echo 'KL' "$(uname -r | awk -F'-' '{print $1}')"
+kernel() {
+  cor-echo 'KL' "$(uname -r | awk -F'-' '{print $1}')"
 }
 
-print-uptime()
-{
+uptime () {
   up=$(</proc/uptime)
-  up=${up//.*}                # string before first . is seconds
-  days=$((${up}/86400))       # seconds divided by 86400 is days
-  hours=$((${up}/3600%24))    # seconds divided by 3600 mod 24 is hours
-  mins=$((${up}/60%60))       # seconds divided by 60 mod 60 is mins
-  color-echo "UP" $days'd '$hours'h '$mins'm'
+  up=${up//.*}                # string antes de . os segundos
+  dias=$((${up}/86400))       # segundos dividido por 86400 são os dias
+  horas=$((${up}/3600%24))    # segundos dividido por 3600 % 24 são as horas
+  minutos=$((${up}/60%60))       # segundos dividido por 60 % 60 são os minutos
+  cor-echo "UP" $dias'd '$horas'h '$minutos'm'
 }
 
-print-shell() {
+shell () {
   shell=$(basename $(echo $SHELL))
-  color-echo 'SH' "$shell"
+  cor-echo 'SH' "$shell"
 }
 
-print-cpu()
-{
+cpu () {
   arm=$(grep ARM /proc/cpuinfo)    # ARM procinfo uses different format
   if [[ "$arm" != "" ]]; then
-    cpu=$(grep -m1 -i 'Processor' /proc/cpuinfo)
+    cpu=$(grep -m1 -i 'Processor' /proc/cpuinfo | awk -F ' ' '{print $1 $2 $3}')
   else
-    cpu=$(grep -m1 -i 'model name' /proc/cpuinfo)
+    cpu=$(grep -m1 -i 'model name' /proc/cpuinfo | awk '{print $6}')
   fi
-  color-echo 'CP' "${cpu#*: }" # everything after colon is processor name
+  cor-echo 'CP' "${cpu#*: }" # everything after colon is processor name
 }
 
-print-gpu()
-{
+gpu () {
   gpu=$(/usr/sbin/lspci | grep VGA | awk -F ': ' '{print $2}' | sed 's/(rev ..)//g')
-  color-echo 'GP' "$gpu"
+  cor-echo 'GP' "$gpu"
 }
 
-print-packages()
-{
+pacotes () {
   packages=$(pacman -Qqs| wc -l)
-  color-echo 'PKG' "$packages"
+  cor-echo 'PK' "$packages"
 }
 
-print-wm()
-{
-	color-echo 'WM' $DESKTOP_SESSION
+wm () {
+	cor-echo 'WM' $DESKTOP_SESSION
 }
 
-print-distro() {
+distro () {
   [[ -e /etc/os-release ]] && source /etc/os-release
   if [[ -n "$NAME" ]]; then
-    color-echo 'OS' "$NAME"
+    cor-echo 'OS' "$NAME"
   else
-    color-echo 'OS' "not found"
+    cor-echo 'OS' "not found"
   fi
 }
 
-print-colors() {
+cores () {
 	for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do
 		cor=$(echo -en "\033[$BG  \033[0m")
 		cores=$cores$cor
 	done
-	echo -ne " ${red}COR: $cores"
+	echo -ne " ${vermelho}CR: $cores"
 }
 
 echo
-echo -en "  █▀▀▀▀▀▀▀▀▀▀▀█   |  " && print-distro
-echo -en "  █           █   |  " && print-wm
-echo -en "  █           █   |  " && print-uptime
-echo -en "  █   █   █   █   |  " && print-shell
-echo -en "  █           █   |  " && print-kernel
-echo -en "  █           █   |  " && print-packages
-echo -en "  ▀█▄▄▄▄▄▄▄▄▄█▀   |  " && print-colors
+echo -en "                  |  " && cpu
+echo -en "  █▀▀▀▀▀▀▀▀▀▀▀█   |  " && distro
+echo -en "  █           █   |  " && wm
+echo -en "  █           █   |  " && uptime
+echo -en "  █   █   █   █   |  " && shell
+echo -en "  █           █   |  " && kernel
+echo -en "  ▀█▄▄▄▄▄▄▄▄▄█▀   |  " && pacotes
+echo -en "                  |  " && cores
 echo
 sleep 10
