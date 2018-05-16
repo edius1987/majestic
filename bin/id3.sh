5656920b0451be1bb083a28d2d778301dcbd2539
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-05-16 17:15:01 -0400

    Autocommit por Git-Cron: alterações: add '.local/bin/screencast' add 'bin/id3.sh'

--- bin/id3.sh
+++ bin/id3.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# id3.sh: Um script para ajustar a tag id3 de várias músicas ao mesmo tempo.
+# id3.sh: Um script para ajustar a tag id3 de várias músicas ao mesmo tempo
+# com base no nome do arquivo.
 #
 # Desenvolvido por Lucas Saliés Brum <lucas@archlinux.com.br>
 #
-# Criado em:           16-09-2017 12:37:00 AMT
-# Última alteração em: 16-09-2017 12:37:00 AMT
+# Criado em:           16/09/2017 12:37:00 AMT
+# Última alteração em: 16/05/2018 17:06:40 
 #
 # Uso: id3.sh /caminho/para/as/mp3s
 
@@ -24,7 +25,7 @@ fi
 
 ls $dir/*.${extensao} 1> /dev/null 2> /dev/null
 if [ $? != 0 ]; then
-    echo "Nenhum arquivo com a extensão: $extensao foi encontrado em ${dir}. Abortando..."
+    echo -en "[*] Nenhum arquivo com a extensão: $extensao foi encontrado em ${dir}.\nAbortando..."
     exit 1
 fi
 
@@ -32,7 +33,7 @@ echo "Tem certeja que deseja alterar todas as tags id3 dos arquivos em ${dir}? [
 read resposta
 
 if [[ "$resposta" != [Ss]* ]]; then
-	echo "Programa abortado pelo usuário..."
+	echo -e "[\033[31m!\e[0m] Programa abortado pelo usuário..."
 	exit 1
 fi
 
@@ -40,8 +41,22 @@ for arquivo in ${dir}/*.${extensao}; do
 	nome=$(basename -s .${extensao} "$arquivo")
 	artista=$(echo "$nome" | awk -F'-' '{print $1}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 	faixa=$(echo "$nome" | awk -F'-' '{$1=""; print}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-	echo "Gravando a id3 de ${arquivo}..."
-	echo "Artista: $artista"
-	echo "Faixa: $faixa"
-	id3 -a "$artista" -t "$faixa" "$arquivo"
+	echo 
+	echo "------------------------------------------------------"
+	echo -e "Gravando a id3 de \033[31m${arquivo}\e[0m..."
+	echo -e "Artista: $artista"
+	echo -e "Faixa: $faixa"
+	echo "------------------------------------------------------"
+
+	if [ "$faixa" ]; then
+		id3 -t "$faixa" "$arquivo"
+	else
+		echo "[[\033[31m!\e[0m]] Impossível determinar título de: \033[31m${arquivo}\e[0m."
+	fi
+
+	if [ "$artista" ]; then
+		id3 -a "$artista" "$arquivo"
+	else
+		echo "[\033[31m!\e[0m] Impossível determinar artista de: \033[31m${arquivo}\e[0m."
+	fi	
 done
