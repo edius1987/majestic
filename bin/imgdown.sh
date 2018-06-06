a55f81aa9f20607d06e36d514419d75b90e72870
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-06-06 14:45:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/imgdown.sh'

--- bin/imgdown.sh
+++ bin/imgdown.sh
@@ -6,6 +6,7 @@ ext="jpg"  	# Separadas por virgula.
 pasta="$(pwd)" 	# Diretório para salvar os arquivos.
 min='100000' 	# Em bytes
 lixeira="${HOME}/.local/share/Trash"
+atual=$(date +'%s')
 
 #[ "$(ls -A $pasta)" ] && echo "Not Empty" || echo "Empty"
 [ "$(ls -A $pasta)" ] && echo "Diretório não-vazio. Abortando..." && exit
@@ -26,6 +27,7 @@ if [ $1 ]; then
 		dominio=$(echo "$u" | awk -F/ '{print $3}')
 		wget --quiet -P $pasta -nd -r -l 1 -H -D $dominio -A $ext "$u"
 	done
+
 else
 	ajuda
 fi
@@ -33,9 +35,15 @@ fi
 for a in $pasta/*.$ext; do
 	if [ -f $a ]; then
 		tamanho=$(stat --printf="%s" $a)
-		if [[ $tamanho -lt $min ]]; then
-			echo "$a tem $tamanho (mínimo: $min), apagando..."
-			mv $a $lixeira
-		fi
+		mod=$(stat -c "%Y" $a)
+		#if [[ $tamanho -lt $min ]]; then
+			echo "$a tem $tamanho (mínimo: $min), checando data..."
+			if [[ $mod -gt $atual ]]; then
+				echo "$a ($mod) é mais novo que $atual, apagando..."
+				#mv $a $lixeira
+			else
+				echo "$a ($mod) é mais velho que $atual, mantendo..."
+			fi
+		#fi
 	fi
 done
