3a67e50452b0f96b91611160b69a597950d17da6
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-06-09 07:00:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/videocut-gui.sh'

--- bin/videocut-gui.sh
+++ bin/videocut-gui.sh
@@ -6,10 +6,37 @@ if [ $? = 1 ]; then
 	exit
 fi
 
+function show_time () {
+    num=(printf '%.*f\n' 0 $1)
+    min=0
+    hour=0
+    day=0
+    if ((num>59)); then
+        ((sec=num%60))
+        ((num=num/60))
+        if((num>59));then
+            ((min=num%60))
+            ((num=num/60))
+            if((num>23));then
+                ((hour=num%24))
+                ((day=num/24))
+            else
+                ((hour=num))
+            fi
+        else
+            ((min=num))
+        fi
+    else
+        ((sec=num))
+    fi
+    echo "${hour}:${min}:${sec}"
+}
+
 video=$1
 
 t=$(ffprobe -i "$video" -show_entries format=duration -v quiet -of csv="p=0")
-total="$(($t / 3600)):$(($t / 60 % 60)):$(($t % 60))"
+#total="$(($t / 3600)):$(($t / 60 % 60)):$(($t % 60))"
+total="$(show_time $t)"
 
 eval $(yad --width=400 --form --field=input:FL --field=start --field=end --field=output:SFL "" "00:00:00" "$total" "" | awk -F'|' '{printf "INPUT=\"%s\"\nSTART=%s\nEND=%s\nOUTPUT=\"%s\"\n", $1, $2, $3, $4}')
 [[ -z $INPUT || -z $START || -z $END || -z $OUTPUT ]] && exit 1
