3aed36e8f606a39d1d794cfef5868645a1aa3ffd
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-06-09 07:30:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/videocut-gui.sh'

--- bin/videocut-gui.sh
+++ bin/videocut-gui.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 command -v yad 1> /dev/null 2> /dev/null 
 if [ $? = 1 ]; then 
@@ -6,10 +6,14 @@ if [ $? = 1 ]; then
 	exit
 fi
 
+nome() {
+	fl=$(basename -- "$1")
+	ext="${fl##*.}"
+	echo "${fl%.*}.$2.${ext}"
+}
+
 function show_time () {
-	#LC_ALL=C
     num=$(LC_ALL=C; echo $(printf '%.*f\n' 0 '475.229000'))
-    #(printf '%.*f' 0 $1)
     min=0
     hour=0
     day=0
@@ -39,20 +43,21 @@ function show_time () {
     echo "${hour}:${min}:${sec}"
 }
 
-video=$1
+if [ "$1" ]; then
+	t=$(ffprobe -i "$1" -show_entries format=duration -v quiet -of csv="p=0")
+	total="$(show_time $t)"
+	nome=$(nome "$1" "novo")
+else
+	total="00:00:00"
+fi
 
-t=$(ffprobe -i "$video" -show_entries format=duration -v quiet -of csv="p=0")
-total="$(show_time $t)"
-
-fl=$(basename -- "$video")
-ext="${fl##*.}"
-fl="${fl%.*}.new.${ext}"
-
-eval $(yad --width=400 --form --field=input:FL --field=start --field=end --field=output:SFL "" "00:00:00" "$total" "$fl" | awk -F'|' '{printf "INPUT=\"%s\"\nSTART=%s\nEND=%s\nOUTPUT=\"%s\"\n", $1, $2, $3, $4}')
+eval $(yad --width=400 --form --field=input:SFL --field=start --field=end --field=output "$1" "00:00:00" "$total" "$nome" | awk -F'|' '{printf "INPUT=\"%s\"\nSTART=%s\nEND=%s\nOUTPUT=\"%s\"\n", $1, $2, $3, $4}')
 [[ -z $INPUT || -z $START || -z $END || -z $OUTPUT ]] && exit 1
 
 DIFF=$(($(date +%s --date="$END")-$(date +%s --date="$START")))
-OFFSET=""$(($DIFF / 3600)):$(($DIFF / 60 % 60)):$(($DIFF % 60))
+#OFFSET=""$(($DIFF / 3600)):$(($DIFF / 60 % 60)):$(($DIFF % 60))
+offset="$(show_time $DIFF)"
 
-#ffmpeg -ss "$START" -t "$OFFSET" -i "$INPUT" "$OUTPUT"
-echo "$START -t $OFFSET -i $INPUT $OUTPUT"
+
+ffmpeg -ss "$START" -t "$offset" -i "$INPUT" "$OUTPUT"
+#echo "$START -t $OFFSET -i" "$(basename -- "$INPUT")" "$(basename -- "$OUTPUT")"
