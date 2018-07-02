814ba09bd5d890af3a918407374381f702137ea4
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-07-01 20:00:02 -0400

    Autocommit por Git-Cron: alterações: add '.config/neofetch/config.conf' add '.config/rofi/scripts/configs' add 'bin/build-st.sh'

--- bin/build-st.sh
+++ bin/build-st.sh
@@ -4,5 +4,5 @@ stdir="${HOME}/aur/st-scrollback-git"
 cd $stdir
 #rm -fr config.def.h st/ st-scrollback-git-*.pkg.tar.xz st-*.diff src/ pkg/
 rm -f st-scrollback-git-*.pkg.tar.xz
-nano config.h
+#nano config.h
 makepkg -isc
