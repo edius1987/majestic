50fa6c6d93b56f73719ab4df51a3f8707f42f91c
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-07-20 12:45:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/videoresize-gui.sh'

--- bin/videoresize-gui.sh
+++ bin/videoresize-gui.sh
@@ -6,7 +6,7 @@
 # Feito por Lucas Saliés Brum, a.k.a. sistematico <lucas@archlinux.com.br>
 #
 # Criado em:        2018-06-09 19:39:27
-# Última alteração: 2018-07-20 12:26:27
+# Última alteração: 2018-07-20 12:42:55
 
 titulo="Video Resize"
 resolucoes=("1280" "1080" "720" "640" "480" "320")
@@ -51,7 +51,36 @@ resolucao=$(echo $resolucao | awk -F'!' '{printf "%s", $2}')
 #[ -f $novo ] && mv "$novo" "$(dirname $novo)/$(nome $novo $$)"
 #ffmpeg -vn -i "${video}" -filter:v scale=${resolucao}:-1 -c:a copy "${novo}" 2>&1 | yad --progress --pulsate
 
-ffmpeg -vn -y -i "${video}" -filter:v scale=$resolucao:-1 -c:a copy "${saida}" | yad --title "$titulo" --text="Redimensionando video..." --width=400 --progress --pulsate --auto-close
+#ffmpeg -vn -y -i "${video}" -filter:v scale=$resolucao:-1 -c:a copy "${saida}" | yad --title "$titulo" --text="Redimensionando video..." --width=400 --progress --pulsate --auto-close
+
+
+# ffframes ()
+# {
+	# same as what you had
+	ff_length1=( $(ffmpeg -i "$video" 2>&1 | sed -n "s/.* Duration: \([^,]*\), start: .*/\1/p") )
+	# here straight awk output of total length
+	ff_length=( $(echo "$ff_length1" | awk -F':'  '{ print $1*3600 + $2*60 + $3 }'))
+		
+	ff_fps=( $(ffmpeg -i "$video" 2>&1 | sed -n "s/.*, \(.*\) tbr.*/\1/p") )
+	# here we have total number of frames
+	total_frames=( $(echo $ff_length $ff_fps | awk '{ printf( "%3.0f\n" ,($1*$2)) } '))
+	
+	#echo total-frames $total_frames 
+# }
+
+
+# ffframes
+
+
+ffmpeg -vn -y -i "${video}" -filter:v scale=$resolucao:-1 -c:a copy "${saida}" 2>&1 | \
+		awk -vRS="\r" '$1 ~ /frame/ {gsub(/frame=/," ");gsub(/fps=/," ");gsub(/kB/," ");gsub(/time=/," ");gsub(/ \(/," ");print "\n#Converting video : '"$fftfile"'.\\n\\nCurrent Frame :\\t\t"$1"\\t\t\tFrame rate (s) :\\t\t\t"$2"\\nFile size (mb) :\\t\t"int($5/1024)"."int((($5/1024)-(int($5/1024)))*10)"\\nDuration of video :\\t'$ff_length1'\\tTime elapsed in video :\\t"int($6/3600)":"int((($6/3600)-(int($6/3600)))*60)":"int((($6/60)-(int($6/60)))*60)"\\nTime Remaining :\\t"int((('$total_frames'-$1)/$2)/3600)":"int((((('$total_frames'-$1)/$2)/3600)-(int((('$total_frames'-$1)/$2)/3600)))*60)":"int((((('$total_frames'-$1)/$2)/60)-(int((('$total_frames'-$1)/$2)/60)))*60)"\\t\tPercent complete :\\t\t"int(($6/'$ff_length')*100) "%";}' | \
+		zenity --progress \
+			--auto-kill \
+			--auto-close \
+			--title="$title" \
+			--width=500
+
+# yad --text "$ff_length1 $ff_length $ff_fps $total_frames" --button=gtk-no:1
 
 if [ $? -eq 0 ]; then
 	yad --text "Video: $saida redimensionado com sucesso." --button=gtk-no:1
