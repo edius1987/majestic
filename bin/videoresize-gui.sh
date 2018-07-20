5dd5568204ee85811d81b68198779e906d1170be
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-07-20 11:00:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/videoresize-gui.sh'

--- bin/videoresize-gui.sh
+++ bin/videoresize-gui.sh
@@ -6,10 +6,10 @@
 # Feito por Lucas Saliés Brum, a.k.a. sistematico <lucas@archlinux.com.br>
 #
 # Criado em:        2018-06-09 19:39:27
-# Última alteração: 2018-07-20 10:37:54
+# Última alteração: 2018-07-20 10:59:54
 
 titulo="Video Resize"
-resolucoes=("720" "640" "480")
+resolucoes=("1280" "1080" "720" "640" "480" "320")
 
 command -v yad 1> /dev/null 2> /dev/null
 if [ $? = 1 ]; then
@@ -27,18 +27,20 @@ caminho() {
     echo $(dirname "${1}")
 }
 
-for r in $resolucoes; do
-    res="$res $(printf "\"$r\"")"
-done
-
+echo "$(echo "${res}" | awk '{$1=$1};1')"
 video=$(yad --title "$titulo" --separator=" " --width=400 --form --field="Arquivo:SFL" | awk '{$1=$1};1')
 [[ -z $video ]] && exit 1
 
 novo=$(dirname "${video}")/$(nome "$video" "resize")
 largura=$(ffprobe -v quiet -show_format -show_streams "${video}" | grep '^width' | cut -d "=" -f 2)
-altura=$(ffprobe -v quiet -show_format -show_streams "${video}" | grep '^height' | cut -d "=" -f 2)
 
-resolucao=$(yad --form --width 300 --entry --title "$titulo" --image=gnome-shutdown --button="gtk-close:1" --button="gtk-ok:0" --field="Resolução:CB" "$res" | awk '{$1=$1};1')
+for r in ${resolucoes[@]}; do
+    if [ $r -lt $largura ]; then
+        res+="$r "
+    fi
+done
+
+resolucao=$(yad --form --width 300 --entry --title "$titulo" --image=gnome-shutdown --button="gtk-close:1" --button="gtk-ok:0" --field="Resolução:CB" $(echo $res | awk '{$1=$1};1'))
 [[ -z $resolucao ]] && exit 1
 
 #[ -f $novo ] && mv "$novo" "$(dirname $novo)/$(nome $novo $$)"
