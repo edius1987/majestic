36cee773ee1615f20311975f542cd08e862ea286
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-07-23 19:00:02 -0400

    Autocommit por Git-Cron: alterações: add 'bin/videoext-gui.sh'

--- bin/videoext-gui.sh
+++ bin/videoext-gui.sh
@@ -5,21 +5,22 @@
 #
 # Feito por Lucas Saliés Brum, a.k.a. sistematico <lucas@archlinux.com.br>
 #
-# Criado em:        2018-07-22 19:39:27
-# Última alteração: 2018-07-23 18:44:41
+# Criado em:        2018-07-22 18:44:41
+# Última alteração: 2018-07-23 18:55:20
 
 # ~/.config/Thunar/uca.xml
 #<action>
 #	<icon>camera-video</icon>
-#	<name>VideoCut</name>
+#	<name>Video Ext</name>
 #	<unique-id>1528543845224954-1</unique-id>
-#	<command>videocut-gui.sh %F</command>
+#	<command>videoext-gui.sh %F</command>
 #	<description></description>
 #	<patterns>*</patterns>
 #	<video-files/>
 #</action>
 
-titulo="Video Cut"
+titulo="Video Ext"
+extensoes=("mp4" "avi" "mov" "mkv")
 
 command -v yad 1> /dev/null 2> /dev/null
 if [ $? = 1 ]; then
@@ -33,53 +34,27 @@ nome() {
     echo "${fl%.*}.$2.$$.${ext}"
 }
 
-function show_time () {
-    num=$(LC_ALL=C; echo $(printf '%.*f\n' 0 "$1"))
-    min=0
-    hour=0
-    day=0
-    if ((num>59)); then
-        ((sec=num%60))
-        ((num=num/60))
-        if((num>59));then
-            ((min=num%60))
-            ((num=num/60))
-            if((num>23));then
-                ((hour=num%24))
-                ((day=num/24))
-            else
-                ((hour=num))
-            fi
-        else
-            ((min=num))
-        fi
-    else
-        ((sec=num))
-    fi
-
-	[[ ${#hour} -lt 2 ]] && hour="0$hour"
-	[[ ${#min} -lt 2 ]] && min="0$min"
-	[[ ${#sec} -lt 2 ]] && sec="0$sec"
-
-    echo "${hour}:${min}:${sec}"
+exts() {
+    fl=$(basename -- "$1")
+    ext="${fl##*.}"
+    echo "${ext}"
 }
 
 if [ "$1" ]; then
     novo=$(dirname "${1}")/$(nome "$1" "cut")
-	t=$(ffprobe -i "$1" -show_entries format=duration -v quiet -of csv="p=0")
-	total="$(show_time $t)"
-	nome=$(nome "$1" "novo")
-else
-	total="00:00:00"
+#	nome=$(nome "$1" "novo")
+
+    for e in ${extensoes[@]}; do
+        if [ "$e" != "$(exts $1)" ]; then
+            ext+="${e}!"
+        fi
+    done
 fi
 
-eval $(yad --title "$titulo" --width=400 --form --field="Arquivo\::SFL" --field="Início:" --field="Fim:" --field="Saída:" "$1" "00:00:00" "$total" "$novo" | awk -F'|' '{printf "entrada=\"%s\"\ninicio=%s\nfim=%s\nsaida=\"%s\"\n", $1, $2, $3, $4}')
-[[ -z $entrada || -z $inicio || -z $fim || -z $saida ]] && exit 1
+eval $(yad --title "$titulo" --width=400 --form --field="Arquivo\::SFL" --field="Extensão:CB" --field="Saída:" "$1" "$(echo ${ext%?} | awk '{$1=$1};1')" "$novo" | awk -F'|' '{printf "entrada=\"%s\"\nextensao=%s\nsaida=\"%s\"\n", $1, $2, $3}')
+[[ -z $entrada || -z $ext || -z $saida ]] && exit 1
 
-DIFF=$(($(date +%s --date="$fim")-$(date +%s --date="$inicio")))
-offset="$(show_time $DIFF)"
-
-(ffmpeg -ss "$inicio" -t "$offset" -i "$entrada" "$saida"  2>&1 | yad --title "$titulo" --progress --pulsate --auto-close --progress-text "Convertendo...")
+(ffmpeg -i "$entrada" "${saida}.${extensao}"  2>&1 | yad --title "$titulo" --progress --pulsate --auto-close --progress-text "Convertendo...")
 
 if [ $? -eq 0 ]; then
     yad --info --title "$titulo" --text "Video: $(basename ${saida}) cortado com sucesso." --button=gtk-ok:1
