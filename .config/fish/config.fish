9205332bdfed7ec569786f29c7d066d9ae953a92
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-07-26 13:15:01 -0400

    Autocommit por Git-Cron: alterações: add '.bash_aliases' add '.config/fish/config.fish'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,6 +55,7 @@ alias showip='curl icanhazip.com'
 alias baixarmp3='youtube-dl --extract-audio --audio-format mp3'
 alias fullsync='rsync -aAXvz --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/var/tmp/","/lost+found",".vzfifo",".cpt_hardlink*",".autorelabel","var/cache/yum"}'
 alias rehash='source ~/.config/fish/config.fish'
+alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 
 # Yay
 alias yay='yay --noconfirm'
