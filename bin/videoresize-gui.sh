d937dffcac2368d297223013782b88ca398dd5a8
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-07-26 00:15:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/videoresize-gui.sh' add 'etc/lightdm/lightdm-gtk-greeter.conf'

--- bin/videoresize-gui.sh
+++ bin/videoresize-gui.sh
@@ -6,14 +6,15 @@
 # Feito por Lucas Saliés Brum, a.k.a. sistematico <lucas@archlinux.com.br>
 #
 # Criado em:        2018-06-09 19:39:27
-# Última alteração: 2018-07-22 21:14:18
+# Última alteração: 2018-07-26 00:02:46
 
 titulo="Video Resize"
 
 #    4:3 aspect ratio resolutions: 640×480, 800×600, 960×720, 1024×768, 1280×960, 1400×1050, 1440×1080 , 1600×1200, 1856×1392, 1920×1440, and 2048×1536.
 #    16:10 aspect ratio resolutions: - 1280×800, 1440×900, 1680×1050, 1920×1200 and 2560×1600.
 #    16:9 aspect ratio resolutions: 1024×576, 1152×648, 1280×720, 1366×768, 1600×900, 1920×1080, 2560×1440 and 3840×2160.
-resolucoes=("1280" "1080" "720" "640" "480" "320")
+resolucoesx=("2560" "1920" "1680" "1440" "1280" "1080" "720" "640" "480" "320")
+resolucoesy=("1600" "1200" "1050" "900" "800" "600" "480" "320")
 
 command -v yad 1> /dev/null 2> /dev/null
 if [ $? = 1 ]; then
@@ -37,8 +38,11 @@ video=$(yad --title "$titulo" --separator=" " --width=400 --form --field="Arquiv
 
 novo=$(dirname "${video}")/$(nome "$video" "resize")
 largura=$(ffprobe -v quiet -show_format -show_streams "${video}" | grep '^width' | cut -d "=" -f 2)
+altura=$(ffprobe -v quiet -show_format -show_streams "${video}" | grep '^height' | cut -d "=" -f 2)
 
-for r in ${resolucoes[@]}; do
+if [ $altura -gt $largura ]; then
+
+for r in ${resolucoesx[@]}; do
     if [ $r -lt $largura ]; then
         res+="${r}!"
     fi
