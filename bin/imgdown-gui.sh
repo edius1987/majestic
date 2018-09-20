c5e4d665104794010a5a10057451bb65ee2026d4
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-09-20 16:45:02 -0400

    Autocommit por Git-Cron: alterações: add 'bin/imgdown-gui.sh'

--- bin/imgdown-gui.sh
+++ bin/imgdown-gui.sh
@@ -13,12 +13,14 @@
 # https://gist.github.com/tayfie/6dad43f1a452440fba7ea1c06d1b603a
 # https://stackoverflow.com/a/19060079
 
+titulo="IMGdown"
 ext="jpg"  		# Separadas por virgula.
 pasta="$(pwd)" 	# Diretório para salvar os arquivos.
 min='300' 		# Resolução Vertical(em pixels)
 lixeira="${HOME}/.local/share/Trash"
 subpasta=$(find "$pasta" -type d | egrep -v 'tmp-' | wc -l)
 temp="tmp-$$"
+icone="gnome-shutdown"
 
 command -v yad 1> /dev/null 2> /dev/null
 if [ $? = 1 ]; then
@@ -31,14 +33,14 @@ if [ $? = 0 ]; then
 	turl="$(xclip -o)"
 fi
 
-eval $(yad --title "IMGdown" --width=400 --form --field="URLs" --field="Resolução Vertical Mínima:" "$turl" "$min" | awk -F'|' '{printf "urls=\"%s\"\nres=%s\n", $1, $2}')
+eval $(yad --title "IMGdown" --window-icon=$icone --width=400 --form --field="URLs" --field="Resolução Vertical Mínima:" "$turl" "$min" | awk -F'|' '{printf "urls=\"%s\"\nres=%s\n", $1, $2}')
 [[ -z $urls || -z $res ]] && exit 1
 
 mkdir $subpasta && mkdir $temp
 
 for u in $urls; do
 	dominio=$(echo "$u" | awk -F/ '{print $3}')
-	(wget --quiet -P "$temp" -nd -r -l 1 -H -D $dominio -A $ext "$u" 2>&1 | yad --title "IMGdown" --progress --wrap --width=400 --auto-close --auto-kill --window-icon=gnome-shutdown --button="gtk-close:0" --image=gnome-shutdown --text "Baixando todos os arquivos com a extensão $ext de $u")
+	(wget --quiet -P "$temp" -nd -r -l 1 -H -D $dominio -A $ext "$u" 2>&1 | yad --title "IMGdown" --progress --wrap --width=400 --auto-close --auto-kill --window-icon=$icone --button="gtk-close:0" --image=gnome-shutdown --text "Baixando todos os arquivos com a extensão $ext de $u")
 	#(wget -P "$pasta" -nd -r -l 1 -H -D $dominio -A $ext "$u" 2>&1 | sed -u 's/.*\ \([0-9]\+%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/' | yad --title "IMGdown" --progress --wrap --width=350 --auto-close --window-icon=gnome-shutdown --button="gtk-close:0" --image=gnome-shutdown --text "Baixando todos os arquivos com a extensão $ext de $u")
 done
 
@@ -46,9 +48,9 @@ for a in $temp/*.$ext; do
 	#tamanho=$(stat --printf="%s" $a)
 	res=$(convert $a -print "%h" /dev/null)
 	if [[ $res -lt $min ]]; then
-		mv $temp/$a $lixeira
+		mv $a $lixeira
 	else
-		mv $temp/$a $subpasta/
+		mv $a $subpasta
 	fi
 done
 
