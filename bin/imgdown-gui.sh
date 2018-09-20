995561f9814d76f2912a1bc356241420ef119295
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-09-20 08:00:02 -0400

    Autocommit por Git-Cron: alterações: add '.gitignore' add 'bin/imgdown.sh' add 'bin/imgdown-gui.sh'

---
+++ bin/imgdown-gui.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# Arquivo: imgdown-gui.sh
+# Descrição: Script usando o YAD, xclip e wget
+# para baixar múltiplos arquivos de uma determinada 
+# extensão em um(ou vários) site(s).
+#
+# Feito por Lucas Saliés Brum, a.k.a. sistematico <lucas@archlinux.com.br>
+#
+# Criado em:        20/09/2018 05:48:46
+# Última alteração: 20/09/2018 05:48:53
+#
+# Referências:
+# https://gist.github.com/tayfie/6dad43f1a452440fba7ea1c06d1b603a
+# https://stackoverflow.com/a/19060079
+
+ext="jpg"  		# Separadas por virgula.
+pasta="$(pwd)" 	# Diretório para salvar os arquivos.
+min='300' 		# Resolução Vertical(em pixels)
+lixeira="${HOME}/.local/share/Trash"
+atual=$(date +'%s')
+subpasta=$(find "$pasta" -type d | wc -l)
+#tamanho=$(echo $min | awk '{ byte =$1 /1024; print byte }')
+
+command -v yad 1> /dev/null 2> /dev/null
+if [ $? = 1 ]; then
+	echo "yad não instalado."
+	exit
+fi
+
+command -v xclip 1> /dev/null 2> /dev/null
+if [ $? = 0 ]; then
+	turl="$(xclip -o)"
+fi
+
+eval $(yad --title "IMGdown" --width=400 --form --field="URLs" --field="Resolução Vertical Mínima:" "$turl" "$min" | awk -F'|' '{printf "urls=\"%s\"\nres=%s\n", $1, $2}')
+[[ -z $urls || -z $res ]] && exit 1
+
+for u in $urls; do
+	dominio=$(echo "$u" | awk -F/ '{print $3}')
+	(wget --quiet -P "$pasta" -nd -r -l 1 -H -D $dominio -A $ext "$u" 2>&1 | yad --title "IMGdown" --progress --pulsate --auto-close --progress-text "Baixando todos os arquivos com a extensão $ext de<br>$u...")
+done
+
+mkdir $subpasta
+
+for a in *.$ext; do
+	#tamanho=$(stat --printf="%s" $a)
+	res=$(convert $a -print "%h" /dev/null)
+	if [[ $res -lt $min ]]; then
+		mv $a $lixeira
+	else
+		mv $a $subpasta/
+	fi
+done
+
+rm -f robots.txt*
--- bin/imgdown.sh
+++ bin/imgdown.sh
@@ -2,33 +2,51 @@
 #
 # https://gist.github.com/tayfie/6dad43f1a452440fba7ea1c06d1b603a
 
-ext="jpg"  	# Separadas por virgula.
+ext="jpg"  		# Separadas por virgula.
 pasta="$(pwd)" 	# Diretório para salvar os arquivos.
 min='100000' 	# Em bytes
 lixeira="${HOME}/.local/share/Trash"
 atual=$(date +'%s')
-subpasta=$(find $pasta -type d | wc -l)
+subpasta=$(find "$pasta" -type d | wc -l)
 
 [ "$(ls -ld $pasta/$subpasta 2> /dev/null)" ] && echo "O diretório $pasta/$subpasta já existe. Abortando..." && exit
 
-if [ "$1" == "-h" ]; then
+if [ "$1" == "-h" ] || [ ! $1 ]; then
     echo "Uso: $(basename $0) \"http://site.com/pagina\""
     echo "$(basename $0) \"http://site.com/pagina\""
     echo "$(basename $0) \"http://site.com/pagina\""
 	exit
 fi
 
-if [ $1 ]; then
-	for u in $@; do
-		echo "Baixando todos os arquivos com a extensão $ext de $u..."
+if [ "$1" == "-g" ]; then
+	command -v yad 1> /dev/null 2> /dev/null
+	if [ $? = 1 ]; then
+		echo "yad não instalado."
+		exit
+	fi
+
+	urls=$(yad --title "IMGdown" --separator=" " --width=400 --form --field="URLs" "")
+	[[ -z $urls ]] && exit 1
+
+	for u in $urls; do
 		dominio=$(echo "$u" | awk -F/ '{print $3}')
-		wget --quiet -P $pasta -nd -r -l 1 -H -D $dominio -A $ext "$u"
+		(wget --quiet -P "$pasta" -nd -r -l 1 -H -D $dominio -A $ext "$u" 2>&1 | yad --title "IMGdown" --progress --pulsate --auto-close --progress-text "Baixando todos os arquivos com a extensão $ext de $u...")
 	done
+
+else
+	if [ $1 ]; then
+		for u in $@; do
+			echo "Baixando todos os arquivos com a extensão $ext de $u..."
+			dominio=$(echo "$u" | awk -F/ '{print $3}')
+			wget --quiet -P "$pasta" -nd -r -l 1 -H -D $dominio -A $ext "$u"
+		done
+	fi
 fi
 
-for a in $pasta/*.$ext; do
-	if [ -f $a ]; then
-		mkdir -p $pasta/$subpasta
+# for a in $pasta/*.$ext; do
+for a in *.$ext; do
+
+		#mkdir -p $pasta/$subpasta
 		tamanho=$(stat --printf="%s" $a)
 		mod=$(stat --printf="%Y" $a)
 		echo
@@ -46,7 +64,6 @@ for a in $pasta/*.$ext; do
 			echo "$a ($mod) é mais velho que $atual, ignorando..."
 		fi
 
-	fi
 done
 
-rm -rf $pasta/robots.txt*
+rm -f robots.txt*
