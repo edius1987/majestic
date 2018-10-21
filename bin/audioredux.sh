77639a848dd54c67c5db0a2662fb4e892b447d93
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-10-20 21:30:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/audioredux.sh'

--- bin/audioredux.sh
+++ bin/audioredux.sh
@@ -1,9 +1,9 @@
 
 #!/usr/bin/env bash
 
-dir="$(pwd)"
-novo="${dir}/${$}"
 titulo="Audio Redux"
+[ $1 ] && dir="$1" || dir="$(pwd)"
+novo="${dir}/${$}"
 
 ext=$(dialog --stdout --title "$titulo" --menu 'Extensão:' 0 0 0   'mp3' '' 'm4a' '')
 if [ $(ls -1 *.$ext 2>/dev/null | wc -l) != 0 ]; then
@@ -13,4 +13,9 @@ if [ $(ls -1 *.$ext 2>/dev/null | wc -l) != 0 ]; then
 	for f in *.$ext; do
 		ffmpeg -i "$f" -codec:v copy -codec:a libmp3lame -q:a $qualidade $novo/"${f%.m4a}.mp3"
 	done
+else
+	dialog --title "$titulo" --msgbox "Nenhum arquivo com a extensão: $ext foi encontrado!" 0 0
+	exit 0
 fi
+
+dialog --title "$titulo" --msgbox "Arquivos reduzidos com sucesso!" 0 0
