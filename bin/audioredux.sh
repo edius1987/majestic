05aa4c16a76f21a654f86ddf7584da96a5e51e01
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-10-20 21:45:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/audioredux.sh'

--- bin/audioredux.sh
+++ bin/audioredux.sh
@@ -5,14 +5,20 @@ titulo="Audio Redux"
 [ $1 ] && dir="$1" || dir="$(pwd)"
 novo="${dir}/${$}"
 
+which dialog 1> /dev/null 2> /dev/null
+if [ $? != 0 ]; then
+	echo "O aplicativo dialog não foi encontrado."
+	exit 1
+fi
+
 ext=$(dialog --stdout --title "$titulo" --menu 'Extensão:' 0 0 0   'mp3' '' 'm4a' '')
 if [ $(ls -1 *.$ext 2>/dev/null | wc -l) != 0 ]; then
 	mkdir $dir
 	qualidade=$(dialog --stdout --title "$titulo" --menu 'Qualidade:' 0 0 0   1 '' 2 '' 3 '')
 
-	for f in *.$ext; do
-		ffmpeg -i "$f" -codec:v copy -codec:a libmp3lame -q:a $qualidade $novo/"${f%.m4a}.mp3"
-	done
+	#for f in *.$ext; do
+		#ffmpeg -i "$f" -codec:v copy -codec:a libmp3lame -q:a $qualidade $novo/"${f%.m4a}.mp3"
+	#done
 else
 	dialog --title "$titulo" --msgbox "Nenhum arquivo com a extensão: $ext foi encontrado!" 0 0
 	exit 0
