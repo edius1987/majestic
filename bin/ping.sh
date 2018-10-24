60441908da8b3acb4448a114b6304b6b9f126d6b
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-10-24 07:30:01 -0400

    Autocommit por Git-Cron: alterações: add 'bin/ping.sh'

--- bin/ping.sh
+++ bin/ping.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# Pacotes necessários: sound-theme-freedesktop & libcanberra
+
 # ls /usr/share/sounds/freedesktop/stereo/
-online=""
-offline=""
+online="phone-incoming-call"
+offline="phone-outgoing-busy"
 
 #h="127.0.0.1"
 #h=${4:-"8.8.8.8"}
@@ -10,22 +12,23 @@ offline=""
 
 h="8.8.8.8"
 r=1
-v=0
+v=1
 l=0
 
 function pingar {
 	ping -q -c$r $h > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]; then
-		if [ "$v" == 1 ]; then
+		if [ $v == 1 ]; then
 			#echo -e "Conexão: \033[0;32mOK\033[0m"
 			echo -e "\033[0;32monline\033[0m"
 		fi
-		export DISPLAY=:0 ; canberra-gtk-play -i phone-incoming-call 2>&1
+		export DISPLAY=:0 ; canberra-gtk-play -i $online 2>&1
 	else
-		if [ "$v" == 1 ]; then
+		if [ $v == 1 ]; then
 			#echo -e "Conexão: \033[0;31mFALHOU\033[0m"
 			echo -e "\033[0;31moffline\033[0m"
 		fi
+		export DISPLAY=:0 ; canberra-gtk-play -i $offline 2>&1
 	fi
 }
 
