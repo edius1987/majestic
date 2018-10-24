80c29021909ff8453e3bbc1d57c702201ae59594
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-10-24 08:45:01 -0400

    Autocommit por Git-Cron: alterações: add '.config/polybar/config' add '.config/polybar/scripts/trash.sh'

--- .config/polybar/scripts/trash.sh
+++ .config/polybar/scripts/trash.sh
@@ -21,6 +21,8 @@ if [[ "${1}" == "-x" ]]; then
 
 	mkdir ${TRASH_DIRECTORY}/files
 	mkdir ${TRASH_DIRECTORY}/info
+
+	export DISPLAY=:0 ; canberra-gtk-play -i trash-empty 2>&1
 elif [[ "${1}" == "-o" ]]; then
 	xdg-open $TRASH_DIRECTORY/files
 fi
@@ -28,9 +30,9 @@ fi
 TRASH_COUNT=$(ls -U -1 "${TRASH_DIRECTORY}/files" | wc -l)
 
 if [[ ${TRASH_COUNT} -gt 0 ]]; then
-	s="%{F#${COR}}%{F-} ${TRASH_COUNT}"
+	s="%{F#${COR}}%{F-} ${TRASH_COUNT}"
 else
-	s="%{F#${COR}}%{F-} 0"
+	s=""
 fi
 
 echo "${s}"
