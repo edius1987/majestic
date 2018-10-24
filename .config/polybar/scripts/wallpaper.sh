e0c9294806aedc79aa040053306606847c95fee1
Lucas Saliés Brum <lucas@archlinux.com.br>
2018-10-24 08:15:01 -0400

    Autocommit por Git-Cron: alterações: add '.config/polybar/scripts/wallpaper.sh' add '.config/polybar/scripts/weather.py' add '.config/rofi/scripts/docker' add '.gitignore'

--- .config/polybar/scripts/wallpaper.sh
+++ .config/polybar/scripts/wallpaper.sh
@@ -5,7 +5,7 @@
 dir="${XDG_PICTURES_DIR:-${HOME}/img}/wallpapers"
 unsplash_dir="${XDG_PICTURES_DIR:-${HOME}/img}/wallpapers/unsplash"
 default="$dir/alex-block-354270-unsplash.jpg"
-ultima="/home/lucas/img/wallpapers/unsplash/james-donovan-414207-unsplash.jpg"
+ultima="/home/lucas/img/wallpapers/deviantart/nude/beautiful_big_butt_by_janeznovak6-dbf5hfg.jpg"
 modo="--bg-fill"
 indice=0
 i=0
